ff095e4e334670b532d4f2c3f15baa7bed3edcb3
Alberto Torres <albertotorresfoltyn@gmail.com>
2020-08-03 01:31:58 -0300

    not much

--- HomeScreen.js
+++ HomeScreen.js
@@ -2,35 +2,39 @@ import * as React from 'react';
 import AuthContext from './AppContext';
 import { Button, Text, View } from 'react-native';
 import data from './response.json'
+import BigButton from './BigButton'
 import { createDrawerNavigator } from '@react-navigation/drawer';
 const Drawer = createDrawerNavigator();
 
+function ExcerciseList({list}) {
+    debugger;
+    return <>
+        {list.map((e,i)=><Text key={i}>{e.name}</Text>)}        
+    </>;
+}
 
-export function ApparatusViewer(apparatus) {
-    return (()=>
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>{apparatus.name}</Text>
+function ApparatusViewer(apparatus) {
+    return  ()=><View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <ExcerciseList list={apparatus.excercises} />
         </View>
-    );
+    
+    
 }
 
 export default function HomeScreen() {
-    debugger;
     const { signOut } = React.useContext(AuthContext);
-
     return (
         <>
-        <Drawer.Navigator
-            drawerStyle={{
-                /*backgroundColor: '#c6cbef',*/
-                width: '45%',
-            }}
-        >
-            {data.map(apparatus=>{
-                return <Drawer.Screen name={apparatus.name} component={ApparatusViewer(apparatus)} />
-            })}
-        </Drawer.Navigator>
-        <Button title="Sign out" onPress={signOut} />
-            </>
+            <Drawer.Navigator
+                drawerStyle={{
+                    width: '45%',
+                }}
+            >
+                {data.map((apparatus, i) => {
+                    return <Drawer.Screen key={i} name={apparatus.name} component={ApparatusViewer(apparatus)} />
+                })}
+            </Drawer.Navigator>
+            <Button title="Sign out" onPress={signOut} />
+        </>
     );
 }
